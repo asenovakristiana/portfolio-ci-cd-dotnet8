a8a2a7ef418ccd0df1da505ada4a9a99578b5e15
Kristiana <asenovakristiana@gmail.com>
2025-06-04 19:12:37 +0300

    NEW

--- obj/Debug/net8.0/PortfolioCiCdDotNet8.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortfolioCiCdDotNet8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioCiCdDotNet8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da14a6d0f093eebc3377ace7be0d1e21316720a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f5ac35c4baccf199d914747a2d099cd2c577f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioCiCdDotNet8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioCiCdDotNet8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
